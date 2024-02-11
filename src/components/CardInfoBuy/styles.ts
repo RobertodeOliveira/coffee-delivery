699698d4b1ec6fc9858ecac400755e8ec653f819
Roberto.Melo <robertomelo287@gmail.com>
2024-02-11 13:11:27 -0300

    card info styles

--- src/components/CardInfoBuy/styles.ts
+++ src/components/CardInfoBuy/styles.ts
@@ -2,7 +2,12 @@ import styled from 'styled-components'
 
 const Container = styled.div`
   padding: 40px;
-  border-color: 1px solid red;
+  border: 1px solid transparent;
+  background-clip: padding-box;
+
+  border-image: linear-gradient(to right, #dbac2c, #8047f8);
+  border-image-slice: 1;
+  border-radius: 6px 36px 6px 36px;
 `
 
 export { Container }
