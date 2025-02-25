85e63e5fd6efe61578880a4c3da4bbc3de8acbcc
Roberto.Melo <robertomelo287@gmail.com>
2025-02-24 22:29:16 -0300

    remove fields

--- src/appStyles.ts
+++
@@ -1,8 +0,0 @@
-import styled, { css } from 'styled-components'
-
-export const Title = styled.h1`
-  ${({ theme }) => css`
-    color: ${theme.colors.brand.yellow};
-    font-size: ${theme['font-size'].xl};
-  `}
-`
--- src/pages/Checkout/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-const Wrapper = styled.div``
-
-export { Container, Wrapper }
