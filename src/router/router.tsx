bbd01acb9bf5353b3cf2ddc018ed1a37e70227fc
Roberto.Melo <robertomelo287@gmail.com>
2025-02-25 23:24:45 -0300

    add basename in router config

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,23 +4,26 @@ import { Home } from '../pages/Home'
 import { Checkout } from '../pages/Checkout'
 import { SucessPage } from './../pages/Sucess/index'
 
-export const routers = createBrowserRouter([
-  {
-    path: '/',
-    element: <Pages />,
-    children: [
-      {
-        path: '/',
-        element: <Home />,
-      },
-      {
-        path: '/checkout',
-        element: <Checkout />,
-      },
-      {
-        path: '/sucess',
-        element: <SucessPage />,
-      },
-    ],
-  },
-])
+export const routers = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Pages />,
+      children: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+        {
+          path: '/checkout',
+          element: <Checkout />,
+        },
+        {
+          path: '/sucess',
+          element: <SucessPage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/coffee-delivery' },
+)
